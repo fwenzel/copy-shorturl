81c39bd287f9d49783fdae232866e3449557368e
Fred Wenzel <fwenzel@mozilla.com>
2011-07-18 14:14:44 -0700

    Removed deprecated on() and postMessage() in content scripts with self.*

--- data/js/find-short-url.js
+++ data/js/find-short-url.js
@@ -3,11 +3,11 @@ const selectors = 'link[rev=canonical],link[rel=shorturl],link[rel=shortlink]';
 
 
 /** Find short URL in the page */
-on('click', function(node, data) {
+self.on('click', function(node, data) {
     let short = document.querySelector(selectors),
         link;
     if (short && (link = short.href)) {
-        postMessage({short: true, url: link});
+        self.postMessage({short: true, url: link});
         return;
     } else {
         // use canonical URL if it exists, current URL otherwise.
@@ -15,6 +15,6 @@ on('click', function(node, data) {
         if (!(canonical && (link = canonical.href)))
             link = document.location.href;
 
-        postMessage({short: false, url: link});
+        self.postMessage({short: false, url: link});
     }
 });
