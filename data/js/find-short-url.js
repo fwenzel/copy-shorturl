ef1ddeb0c85db863b6b1dba1259df8220f8c0d4c
Hector <l-hedgehog@126.com>
2011-07-30 16:00:45 +0800

    limit hash appending to permlink

--- data/js/find-short-url.js
+++ data/js/find-short-url.js
@@ -5,9 +5,10 @@ const selectors = 'link[rev=canonical],link[rel=shorturl],link[rel=shortlink]';
 /** Find short URL in the page */
 self.on('click', function(node, data) {
     let short = document.querySelector(selectors),
+        hash = node.hash ? node.hash : '',
         link;
     if (short && (link = short.href)) {
-        self.postMessage({short: true, url: (link + document.location.hash)});
+        self.postMessage({short: true, url: link + hash});
         return;
     } else {
         // use canonical URL if it exists, current URL otherwise.
@@ -15,6 +16,6 @@ self.on('click', function(node, data) {
         if (!(canonical && (link = canonical.href)))
             link = document.location.href;
 
-        self.postMessage({short: false, url: link});
+        self.postMessage({short: false, url: link + hash});
     }
 });
