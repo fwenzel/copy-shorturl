4e11a7628a000a7366a36bbdd0204045f0fcbd0e
Hector <l-hedgehog@126.com>
2011-09-19 23:13:42 +0800

    limit hash appending to permlink

--- data/js/find-short-url.js
+++ data/js/find-short-url.js
@@ -5,15 +5,16 @@ const selectors = 'link[rev=canonical],link[rel=shorturl],link[rel=shortlink]';
 /** Find short URL in the page */
 self.on('click', function(node, data) {
     let short = document.querySelector(selectors),
+        hash = node.hash ? node.hash : '',
         link;
-    if (short && (link = short.href)) {
-        self.postMessage({short: true, url: (link + document.location.hash)});
+    if (short && (link = short.href + hash)) {
+        self.postMessage({short: true, url: link});
         return;
     } else {
         // use canonical URL if it exists, current URL otherwise.
         let canonical = document.querySelector('link[rel=canonical]');
-        if (!(canonical && (link = canonical.href)))
-            link = document.location.href;
+        if (!(canonical && (link = canonical.href + hash)))
+            link = hash ? node.href : document.location.href;
 
         self.postMessage({short: false, url: link});
     }
