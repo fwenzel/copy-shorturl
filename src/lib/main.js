c9e39d8d8a8a28da2936053e3c48e50b11fadda5
Fred Wenzel <code@wenzel.io>
2017-05-29 11:11:31 -0700

    keyboard shortcut and link URL shortening

--- src/lib/main.js
+++ src/lib/main.js
@@ -4,23 +4,50 @@ const _ = browser.i18n.getMessage;
 
 
 /** Add context menus */
+// per-page
 browser.contextMenus.create({
   id: 'shorten-page',
   title: _('menuitem_label'),
   contexts: ['page']
 });
 
+// per-link
+browser.contextMenus.create({
+  id: 'shorten-link',
+  title: _('shorten_link_label'),
+  contexts: ['link']
+});
+
+
 /** Process content menu clicks */
 browser.contextMenus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
-    case 'shorten-page':
-      browser.tabs.executeScript({
-        file: '/data/js/find-short-url.js',
-        matchAboutBlank: true
-      });
-      break;
+  case 'shorten-page':
+    discoverUrl();
+    break;
+  case 'shorten-link':
+    processUrl({
+      url: info.linkUrl,
+      short: false
+    });
+    break;
   }
 });
 
-/** Handle incoming short URLs. */
+/** Listen to keyboard shortcut. */
+browser.commands.onCommand.addListener((cmd) => {
+  if (cmd === 'shorten-page-url') {
+    discoverUrl();
+  }
+});
+
+/**
+ * Execute content script to discover canonical short URL on current tab,
+ * then listen to it being returned via message.
+ */
+function discoverUrl() {
+  browser.tabs.executeScript({
+    file: '/data/js/find-short-url.js'
+  });
+}
 browser.runtime.onMessage.addListener(processUrl);
--- to_webextension.md
+++ to_webextension.md
@@ -3,10 +3,11 @@
 [x] Enable context menu on all pages
 [x] shorten URL
 [x] notify on success
-[ ] context menu on a tags
-[ ] accel-shift-l shortcut
-[ ] toolbar button (pref)
+[x] context menu on links
+[x] accel-shift-l shortcut
+[ ] toolbar button (with pref?)
+[x] strip urm_ crap
 [ ] prefs: choose shortener
 [ ] prefs: use custom shortener URI
-[ ] i18n
-[x] strip urm_ crap
+[ ] prefs: urm_
+[ ] i18n throughout
