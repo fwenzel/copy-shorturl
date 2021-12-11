f940d458e5f9adb8dc85c55210f77eccbad114fe
Fred Wenzel <code@wenzel.io>
2021-12-11 10:54:36 -0800

    Fix special URL generation for amazon.com. Fix #129.

--- src/data/js/discover/amazon.js
+++ src/data/js/discover/amazon.js
@@ -2,11 +2,11 @@
   /** Find short URL in the page */
   function findShortUrl() {
     // Example: https://www.amazon.com/dp/ABC012345DE
-    const matches = document.location.href.match(new RegExp('/dp/(\\w+)'));
+    const matches = document.location.href.match(new RegExp('(/dp/\\w+)'));
 
     if (matches) {
       const title = document.title;
-      const shortened = new URL(`/${matches[1]}`, 'https://amzn.com');
+      const shortened = new URL(matches[1], 'https://amzn.com');
 
       return {
         short: true,
